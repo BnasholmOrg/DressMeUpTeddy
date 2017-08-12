a6b9abb4498bd20d24efed9793de9e2a0894cccb
bnasholm <breannanasholm@gmail.com>
2017-08-11 17:04:25 -0700

    Initial Commit

---
+++ DressMeUpTeddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DressMeUpTeddy
+//
+//  Created by Breanna Nasholm on 8/11/17.
+//  Copyright © 2017 Breanna Nasholm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
