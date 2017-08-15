c85481f9dbeb2d1d4a9afb229ce2d70388b2d727
bnasholm <breannanasholm@gmail.com>
2017-08-15 14:02:14 -0700

    Drag and Drop of clothing items works properly now

--- DressMeUpTeddy/ViewController.swift
+++ DressMeUpTeddy/ViewController.swift
@@ -20,15 +20,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var originalCenter = CGPoint.zero
+    
     @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: self.view)
-        if let view = recognizer.view {
-            view.center = CGPoint(x:view.center.x + translation.x,
-                                  y:view.center.y + translation.y)
+        switch(recognizer.state){
+        case UIGestureRecognizerState.began:
+            if let originalView = recognizer.view {
+            originalCenter = originalView.center
+            print("Original Center \(originalCenter)")
+            }
+        case UIGestureRecognizerState.changed:
+            let translation = recognizer.translation(in: self.view)
+            if let view = recognizer.view {
+                view.center = CGPoint(x:view.center.x + translation.x,
+                                      y:view.center.y + translation.y)
+            }
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+        case UIGestureRecognizerState.ended:
+            if let newView = recognizer.view {
+                print("OriginalCenter.x \(originalCenter.x)")
+                newView.center = originalCenter
+                print ("New position \(newView.center)")
+            }
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+        default: break
         }
-        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
-
 
 }
 
